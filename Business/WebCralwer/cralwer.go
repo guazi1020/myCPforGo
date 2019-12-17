7c1d48ca386d54efc49b4e9ae773c9ea7cfed530
guazi1020@yeah.net <guazi1020@yeah.net>
2019-12-17 23:50:23 +0800

    1

--- Business/WebCralwer/cralwer.go
+++ Business/WebCralwer/cralwer.go
@@ -43,40 +43,77 @@ func GetWeb() {
 				}
 			}
 		})
+		//比赛结果
+		dom.Find("span.boldbf").Each(func(i int,s *goquery.Selection){
+			//fmt.Println(i,s.Text())
+			games[i].GresultScore= Com.RemoveBlank(s.Text())
+		})
+		//赛制
+		dom.Find("body>span[style]").Each(func(i int,s *goquery.Selection){
+			//fmt.Println(i,s.Text())
+			games[i].Gleague= Com.RemoveBlank(s.Text())
+			fmt.Println(s.Html())
+			//s.Find
+		})
 
-		// dom.Find("span").Each(func(i int,s *goquery.Selection){
-		// 	fmt.Println(s.Text())
-		// })
-		// dom.Find("span.sptr a").Each(func(i int, s *goquery.Selection) {
-		// 	fmt.Println(s.Text())
-		// })
-		for _, value := range games {
-			fmt.Println(value)
-		}
+		dom.Find("span.sptl").Children().Each(func(i int, s *goquery.Selection) {
+			_, bl := s.Attr("href")
+			if bl {
+				//fmt.Println("编号", i)
+				games[(i)/4].GguestName = s.Text()
+			}
+			 strClass, result := s.Attr("class")
+			 if result == true {
+			 	switch strClass {
+				 case "hongpai": //客队比赛红牌
+				 // fmt.Println("编号", i)
+			 		games[(i-3)/4].GredQuantitlesGuest = Com.RemoveBlank(s.Text())
+			 		break
+				 case "paim": //客队排名	
+					 games[(i-1)/4].GguestRank = Com.RemoveCharacter(Com.RemoveCharacter(s.Text(), "", "]"), "", "[")
+			 	default:
+			 		break
+			 	}
+			 }
+		})
+		//半场比分
+		dom.Find("span.bcbf").Each(func(i int,s *goquery.Selection){
+			games[i].GresultHalfScore= Com.RemoveBlank(s.Text())
+		})
+		//最终结果
+		dom.Find("strong.f_sf").Each(func(i int,s *goquery.Selection){
+			games[i].Gresult=Com.RemoveBlank(s.Text())
+		})
+		//是否完成比赛
+		dom.Find("strong").Each(func(i int,s *goquery.Selection){
+			strclass,_:=s.Attr("class")
+			if strclass=="red"{
+				games[i/8].GIsfinish=Com.RemoveBlank(s.Text())
+			}
+		})
+		//比赛欧赔
+		dom.Find("div.oupei").Each(func(i int,s *goquery.Selection){
+			s.Find("span").Each(func(j int,t *goquery.Selection){
+				switch j{
+				case 0:
+					games[i].GspWin=Com.RemoveBlank(t.Text())			
+					break
+				case 1:
+					//fmt.Println(i,t.Text())
+					games[i].GspTie=Com.RemoveBlank(t.Text())
+					break
+				case 2:
+					games[i].GspDefeat=Com.RemoveBlank(t.Text())
+					break
+				default:
+					break
+				}
+			})
+		})
+		// for index, value := range games {
+		// 	fmt.Println(index,value)
+		// }
 
-		// //fmt.Println(dom.Html())
-		// dom.Find("em").Siblings().Each(func(i int, s *goquery.Selection) {
-		// 	class, _ := s.Attr("class")
-		// 	if class == "input-xh" {
-		// 		fmt.Println(class)
-		// 	}
-		// 	fmt.Println(s.Find("a").Html())
-		// 	// fmt.Println(s.Html())
-		// 	// var game Model.Game
-
-		// 	// switch class {
-		// 	// case "input-xh":
-		// 	// 	game.Gnumber = s.Text()
-		// 	// 	break
-		// 	// case "paim":
-		// 	// 	fmt.Println(s.Text())
-		// 	// 	game.GhomeRank = Com.RemoveBlank(s.Text())
-		// 	// 	break
-		// 	// default:
-		// 	// 	break
-		// 	// }
-		// 	//game.SaveGametoDB()
-		// })
 	})
 	c.OnHTML("tr", func(e *colly.HTMLElement) {
 		//log.Println(e.ChildAttr(".matchDate", "date"))
