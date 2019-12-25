61cd17899efdb3639f4bcd0652ddb0e971937817
guazi1020 <guazi1020@yeah.net>
2019-12-25 13:25:35 +0800

    25

--- Business/WebCralwer/saveDb.go
+++ Business/WebCralwer/saveDb.go
@@ -9,9 +9,10 @@ const base_format = "2006-01-02 15:04:05"
 
 func SaveDBTodey() {
 	//fmt.Println(time.Now(time.Now().Year(), time.Now().Month(), time.Now().Day()))
+
 	t := time.Now()
 	str_time := t.Format(base_format)
 	fmt.Println(str_time)
 	//uuid的产生
-	
+
 }
