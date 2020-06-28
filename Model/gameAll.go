205bb808cd39c3a845dd8acc8c4fa10c2470338e
guazi1020@yeah.net <guazi1020@yeah.net>
2020-06-28 23:14:26 +0800

    20200628

---
+++ Model/gameAll.go
@@ -0,0 +1,30 @@
+package Model
+
+type GameAll struct {
+	//主键 UUID
+	UUID string
+	//赛事
+	GAleague string
+	//轮数
+	GARound string
+	//比赛时间
+	GADate string
+	//主队排名
+	GAHomeRank string
+	//主队名称
+	GAHomeName string
+	//客队排名
+	GAGuestRank string
+	//客队名称
+	GAGuestHome string
+	//比分
+	GAresultScore string
+	//比赛结果
+	GAresult string
+	//GspWin sp胜
+	GAspWin string
+	//sp平
+	GAspTie string
+	//sp负
+	GAspDefeat string
+}
