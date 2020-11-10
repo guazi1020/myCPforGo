b51d6295e1266a5d3a5f9cf6ff5ee7c3bdaadf25
guazi1020 <guazi1020@yeah.net>
2020-11-10 13:59:52 +0800

    20201110

--- Test/main_test.go
+++ Test/main_test.go
@@ -1,14 +1,12 @@
 package Test
 
 import (
-	"myCPforGo/Business/CPHttp/ImpMethod"
-	"myCPforGo/Interface/HTTP"
 	"testing"
 )
 
 func TestA(t *testing.T) {
 	// var httpMode HTTP.IGetGameData
-	// httpMode = ImpMethod.GetGameDataOne{Year: "2020"}
+	// htt pMode = ImpMethod.GetGameDataOne{Year: "2020"}
 	// httpMode.GetGameDataForYear()
 	//
 }
